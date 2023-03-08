1a3d9f9ac94faedacf0090680c686996ac2911de
Ruben Adisuryo <67088252+yungbenn@users.noreply.github.com>
2023-03-09 01:16:08 +0700

    first commit

--- serieA-fetch.js
+++ serieA-fetch.js
@@ -1,28 +1,37 @@
 const prompt = require('prompt-sync')({ sigint: true });
 
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '883443cac9mshefaf525d2223f08p17b70djsnb5117a1945b2',
-		'X-RapidAPI-Host': 'serie-a-standings.p.rapidapi.com'
-	}
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '883443cac9mshefaf525d2223f08p17b70djsnb5117a1945b2',
+    'X-RapidAPI-Host': 'serie-a-standings.p.rapidapi.com',
+  },
 };
 
 function menu() {
-  console.log('\x1b[33m%s\x1b[0m', '----------------------------------------------------------');
+  console.log(
+    '\x1b[33m%s\x1b[0m',
+    '----------------------------------------------------------',
+  );
   console.log('\x1b[33m%s\x1b[0m', 'Serie A Standings\n');
   console.log('\x1b[33m%s\x1b[0m', '1. Top 20 Serie A standings');
   console.log('\x1b[33m%s\x1b[0m', '2. Serie A clubs that qualified to UCL');
-  console.log('\x1b[33m%s\x1b[0m', '3. Serie A clubs that is likely to be relegated');
+  console.log(
+    '\x1b[33m%s\x1b[0m',
+    '3. Serie A clubs that is likely to be relegated',
+  );
   console.log('\x1b[33m%s\x1b[0m', '4. Exit');
-  console.log('\x1b[33m%s\x1b[0m', '----------------------------------------------------------\n');
+  console.log(
+    '\x1b[33m%s\x1b[0m',
+    '----------------------------------------------------------\n',
+  );
 }
 
 fetch('https://serie-a-standings.p.rapidapi.com/', options)
-	.then(res => res.json())
-	.then(res => {
+  .then((res) => res.json())
+  .then((res) => {
     while (true) {
-      menu()
+      menu();
       var chooseMenu = prompt('Choose menu: ');
       if (chooseMenu == 1) {
         console.log('\x1b[32m%s\x1b[0m', 'Ranking\t\tSerie A');
@@ -52,4 +61,4 @@ fetch('https://serie-a-standings.p.rapidapi.com/', options)
       }
     }
   })
-	.catch(err => console.error(err));
+  .catch((err) => console.error(err));
