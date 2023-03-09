2e2b77b5a095c2dab56d5a914771f47a65e47b5c
Ruben Adisuryo <67088252+yungbenn@users.noreply.github.com>
2023-03-09 16:32:48 +0700

    cleaning

--- serieA-fetch.js
+++ serieA-fetch.js
@@ -8,14 +8,17 @@ const options = {
   },
 };
 
+const yellow = '\x1b[33m%s\x1b[0m';
+const cyan = '\x1b[32m%s\x1b[0m';
+
 function menu() {
-  console.log('\x1b[33m%s\x1b[0m', '----------------------------------------------------------');
-  console.log('\x1b[33m%s\x1b[0m', 'Serie A Standings\n');
-  console.log('\x1b[33m%s\x1b[0m', '1. Top 20 Serie A standings');
-  console.log('\x1b[33m%s\x1b[0m', '2. Serie A clubs that qualified to UCL');
-  console.log('\x1b[33m%s\x1b[0m', '3. Serie A clubs that is likely to be relegated');
-  console.log('\x1b[33m%s\x1b[0m', '4. Exit');
-  console.log('\x1b[33m%s\x1b[0m', '----------------------------------------------------------\n');
+  console.log(yellow, '----------------------------------------------------------');
+  console.log(yellow, 'Serie A Standings\n');
+  console.log(yellow, '1. Top 20 Serie A standings');
+  console.log(yellow, '2. Serie A clubs that qualified to UCL');
+  console.log(yellow, '3. Serie A clubs that is likely to be relegated');
+  console.log(yellow, '4. Exit');
+  console.log(yellow, '----------------------------------------------------------\n');
 }
 
 fetch('https://serie-a-standings.p.rapidapi.com/', options)
@@ -25,21 +28,21 @@ fetch('https://serie-a-standings.p.rapidapi.com/', options)
       menu();
       var chooseMenu = prompt('Choose menu: ');
       if (chooseMenu == 1) {
-        console.log('\x1b[32m%s\x1b[0m', 'Ranking\t\tSerie A');
+        console.log(cyan, 'Ranking\t\tSerie A');
         for (let n = 0; n < 20; n++) {
           console.log(`${res[n].stats.rank}\t\t${res[n].team.name}`);
         }
         console.log(' ');
         continue;
       } else if (chooseMenu == 2) {
-        console.log('\x1b[32m%s\x1b[0m', 'Ranking\t\tSerie A');
+        console.log(cyan, 'Ranking\t\tSerie A');
         for (let n = 0; n < 4; n++) {
           console.log(`${res[n].stats.rank}\t\t${res[n].team.name}`);
         }
         console.log(' ');
         continue;
       } else if (chooseMenu == 3) {
-        console.log('\x1b[32m%s\x1b[0m', 'Ranking\t\tSerie A');
+        console.log(cyan, 'Ranking\t\tSerie A');
         for (let n = 17; n < 20; n++) {
           console.log(`${res[n].stats.rank}\t\t${res[n].team.name}`);
         }
